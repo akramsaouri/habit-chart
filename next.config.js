8f3191eee1e8fbea4ebbd4b100f74335fdeb0460
akramsaouri <ak.saouri@gmail.com>
2022-05-23 20:31:22 +0200

    update default year

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 source: "/",
-                destination: "/2021",
+                destination: "/2022",
                 permanent: false,
             },
         ];
